a1e96702f7b01cbb3fd477d2a5151ab8795766e6
Christian Bongard <christian.bongard90@gmail.com>
2022-09-27 09:20:24 +0400

    intro to DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log("Hello from src/index.js!");
+
+const list = document.querySelector("#players"); // CSS id selector
+
+const listID = document.getElementById("players");
+
+console.log(list);
+console.log(listID);
+
+let text = document.querySelector("p");
+console.log(text);
+text = document.querySelector("body .red");
+console.log(text);
+
+list.insertAdjacentHTML("beforeend", "<li>Luke</li>");
+list.insertAdjacentHTML("beforeend", "<li>Anakin</li>");
+
+const fifaWinners = document.getElementById("fifa-wins");
+let winners = fifaWinners.querySelectorAll("li");
+
+console.log(winners);
+
+winners.forEach((winner) => {
+  console.log(winner.innerText);
+});
+
+fifaWinners.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
